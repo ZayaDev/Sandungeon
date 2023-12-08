234c2251b6e382f8835cc0565db5de3bc7cb1208
Zay <editzkaneki24@gmail.com>
2023-12-08 17:18:52 -0300

    create player and goblin class

---
+++ Sandungeon/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandungeon
+{
+    class Entity
+    {
+        int life;
+        bool near = false;
+
+
+        protected void walk ()
+        {
+            if (near == false)
+            {
+                near = true;
+            } else
+            {
+                near = false;
+            }
+        }
+    }
+}
